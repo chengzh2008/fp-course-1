277cd1fcb68286491b8c8efa906ed2106744d113
Chengzh2008 <>
2018-06-21 15:46:39 -0700

    review ExactlyOne and Validation

--- src/Course/Core.hs
+++ src/Course/Core.hs
@@ -47,75 +47,21 @@ module Course.Core(
 ) where
 
 
-import Prelude(
-    Eq(..)
-  , Ord(..)
-  , Show(..)
-  , Integral(..)
-  , RealFrac(..)
-  , Num(..)
-  , Fractional(..)
-  , Bool(..)
-  , Either(..)
-  , Char
-  , Int
-  , Integer
-  , IO
-  , Rational
-  , seq
-  , error
-  , undefined
-  , const
-  , flip
-  , curry
-  , uncurry
-  , id
-  , otherwise
-  , (.)
-  , ($)
-  , (&&)
-  , (||)
-  , not
-  , even
-  , odd
-  , fst
-  , snd
-  )
-import Data.String(
-  IsString(..)
-  )
-
-import System.IO(
-    getChar
-  )
-import Data.Function(
-    on
-  )
-import Control.Arrow(
-    first
-  , second
-  , (&&&)
-  , (***)
-  )
+import Prelude
+       (Bool(..), Char, Either(..), Eq(..), Fractional(..), IO, Int,
+        Integer, Integral(..), Num(..), Ord(..), Rational, RealFrac(..),
+        Show(..), ($), (&&), (.), (||), const, curry, error, even, flip,
+        fst, id, not, odd, otherwise, seq, snd, uncurry, undefined)
+import Data.String (IsString(..))
+import System.IO (getChar)
+import Data.Function (on)
+import Control.Arrow ((&&&), (***), first, second)
 import Data.Char
 
-ifThenElse ::
-  Bool
-  -> a
-  -> a
-  -> a
-ifThenElse True t _ =
-  t
-ifThenElse False _ f =
-  f
+ifThenElse :: Bool -> a -> a -> a
+ifThenElse True t _ = t
+ifThenElse False _ f = f
 
-bool ::
-  a
-  -> a
-  -> Bool
-  -> a
-bool f _ False =
-  f
-bool _ t True =
-  t
-
+bool :: a -> a -> Bool -> a
+bool f _ False = f
+bool _ t True = t
